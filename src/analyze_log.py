7f4230fcca52edc0d662448d9156641bcb60328b
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-03-21 03:01:00 +0000

    feat: req1 - generate info for ppublicity campaign

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,31 +1,37 @@
 import csv
+import os
 
 
-def analyze_log(path_to_file: str):
-    if not path_to_file.lower().endswith('.csv'):
+def analyze_log(path_to_file):
+
+    if not path_to_file.endswith('.csv'):
         raise FileNotFoundError(f"Extensão inválida: '{path_to_file}'")
 
-    try:
-        with open(path_to_file, 'r') as f:
-            orders = f.read().splitlines()
-    except FileNotFoundError:
+    if not os.path.isfile(path_to_file):
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
-with open(path_to_file) as file:
-    orders = list(csv.reader(file))
-    items = {order[1] for order in orders}
-    days = {order[2] for order in orders}
+    with open(path_to_file) as file:
+        orders = list(csv.reader(file))
+        items = {order[1] for order in orders}
+        days = {order[2] for order in orders}
 
-    arnaldo_hamburger_orders = sum(1 for order in orders if order[0] == 'arnaldo' and order[1] == 'hamburguer')
+        arnaldo_hamburger_orders = sum(
+            1 for order in orders if order[0] == 'arnaldo'
+            and order[1] == 'hamburguer')
 
-    joao_ordered_items = {order[1] for order in orders if order[0] == 'joao'}
-    joao_never_ordered_dishes = items - joao_ordered_items
+        joao_ordered_items = {order[1] for order in orders if order[0] == 'joao'}
+        joao_never_ordered_items = items - joao_ordered_items
+        joao_ordered_days = {order[2] for order in orders if order[0] == 'joao'}
+        days_joao_never_ordered = days - joao_ordered_days
 
-    joao_ordered_days = {order[2] for order in orders if order[0] == 'joao'}
-    days_joao_never_ordered = days - joao_ordered_days
+        maria_items = [item for item in orders if item[0] == 'maria']
+        maria_most_ordered_item = max(
+            set(item[1] for item in maria_items),
+            key=[item[1] for item in maria_items].count
+        )
 
-    maria_items = [order for order in orders if order[0] == 'maria']
-    maria_most_ordered_dish = max(set(item[1] for item in maria_items), key=[item[1] for item in maria_items].count)
-
-with open('data/mkt_campaign.txt', "w") as file:
-    print(maria_most_ordered_dish, arnaldo_hamburger_orders, joao_never_ordered_dishes, days_joao_never_ordered, file=file)
+    with open('data/mkt_campaign.txt', "w") as file:
+        print(maria_most_ordered_item, file=file)
+        print(arnaldo_hamburger_orders, file=file)
+        print(joao_never_ordered_items, file=file)
+        print(days_joao_never_ordered, file=file)
