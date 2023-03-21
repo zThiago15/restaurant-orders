f23eb7840ddd13e4176ead1535cd523556252ae3
Thiago Dias <thiaguinhodias.15@gmail.com>
2023-03-21 02:46:33 +0000

    feat: req2 - track orders that generates continuous info life favorite dish per client

--- src/analyze_log.py
+++ src/analyze_log.py
@@ -1,4 +1,3 @@
-from typing import Dict, Set
 import csv
 
 
@@ -12,36 +11,21 @@ def analyze_log(path_to_file: str):
     except FileNotFoundError:
         raise FileNotFoundError(f"Arquivo inexistente: '{path_to_file}'")
 
-    with open(path_to_file) as file:
-        orders = list(csv.reader(file))
-        items = {order[1] for order in orders}
-        days = {order[2] for order in orders}
+with open(path_to_file) as file:
+    orders = list(csv.reader(file))
+    items = {order[1] for order in orders}
+    days = {order[2] for order in orders}
 
-        arnaldo_hamburger_orders = 0
-        for order in orders:
-            if order[0] == 'arnaldo' and order[1] == 'hamburguer':
-                arnaldo_hamburger_orders += 1
+    arnaldo_hamburger_orders = sum(1 for order in orders if order[0] == 'arnaldo' and order[1] == 'hamburguer')
 
-        joao_ordered_items = set()
-        for order in orders:
-            if order[0] == 'joao':
-                joao_ordered_items.add(order[1])
-        joao_never_ordered_dishes = items - joao_ordered_items
+    joao_ordered_items = {order[1] for order in orders if order[0] == 'joao'}
+    joao_never_ordered_dishes = items - joao_ordered_items
 
-        joao_ordered_days = set()
-        for order in orders:
-            if order[0] == 'joao':
-                joao_ordered_days.add(order[2])
-        days_joao_never_ordered = days - joao_ordered_days
+    joao_ordered_days = {order[2] for order in orders if order[0] == 'joao'}
+    days_joao_never_ordered = days - joao_ordered_days
 
-        maria_items = []
-        for order in orders:
-            if order[0] == 'maria':
-                maria_items.append(order)
-        maria_most_ordered_dish = max(set(item[1] for item in maria_items), key=[item[1] for item in maria_items].count)
+    maria_items = [order for order in orders if order[0] == 'maria']
+    maria_most_ordered_dish = max(set(item[1] for item in maria_items), key=[item[1] for item in maria_items].count)
 
-    with open('data/mkt_campaign.txt', "w") as file:
-        print(maria_most_ordered_dish, file=file)
-        print(arnaldo_hamburger_orders, file=file)
-        print(joao_never_ordered_dishes, file=file)
-        print(days_joao_never_ordered, file=file)
+with open('data/mkt_campaign.txt', "w") as file:
+    print(maria_most_ordered_dish, arnaldo_hamburger_orders, joao_never_ordered_dishes, days_joao_never_ordered, file=file)
--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,36 @@
 class TrackOrders:
+    def __init__(self):
+        self.orders = []
+
     # aqui deve expor a quantidade de estoque
     def __len__(self):
-        pass
+        return len(self.orders)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self.orders.append({
+            "customer": customer,
+            "order": order,
+            "day": day
+        })
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        dishes = [order["order"] for order in self.orders if order["customer"] == customer]
+        return max(set(dishes), key=dishes.count)
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        all_dishes = set(order["order"] for order in self.orders)
+        ordered_dishes = set(order["order"] for order in self.orders if order["customer"] == customer)
+        return all_dishes - ordered_dishes
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        all_days = set(order["day"] for order in self.orders)
+        visited_days = set(order["day"] for order in self.orders if order["customer"] == customer)
+        return all_days - visited_days
 
     def get_busiest_day(self):
-        pass
+        days = [order["day"] for order in self.orders]
+        return max(set(days), key=days.count)
 
     def get_least_busy_day(self):
-        pass
+        days = [order["day"] for order in self.orders]
+        return min(set(days), key=days.count)
